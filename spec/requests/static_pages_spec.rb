66ea9f2ead4120460128dfe8570d195f744ccebd
melonsoda5ml <mitsuba@sfc.wide.ad.jp>
2014-08-07 01:34:59 +0900

    test page titles with rspec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,17 @@
 require 'spec_helper'
+
 describe "Static pages" do
 
   describe "Home page" do
+
 		it "should have the content 'Sample App'" do
 			visit '/static_pages/home'
 			expect(page).to have_content('Sample App')
+		end
+
+		it "should have the content 'Home'" do
+			visit '/static_pages/home'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
 		end
 	end
 
@@ -13,12 +20,20 @@ describe "Static pages" do
 			visit '/static_pages/help'
 			expect(page).to have_content('Help')
 		end
+		it "should have the content 'Help'" do
+			visit '/static_pages/help'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+		end
 	end
 
 	describe "About page" do
 		it "should have the content 'About Us' " do
 			visit '/static_pages/about'
 			expect(page).to have_content('About Us')
+		end
+		it "should have the content 'About Us'" do
+			visit '/static_pages/about'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
 		end
 	end
 end
