dfb92e272db07a91a0e8690e72c49e368b637658
melonsoda5ml <mitsuba@sfc.wide.ad.jp>
2014-08-26 13:43:12 +0900

    error partion

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -12,7 +12,7 @@ describe "User Pages" do
 	end
 
   describe "signup page" do
-		before {visit signup_path }
+		before {visit signup_path}
 		it {should have_content('Sign up')}
 		it {should have_title(full_title('Sign up'))}
   end
