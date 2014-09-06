b9fbe2ee50e07a81c3fbce649cbfbe534355fe18
melonsoda5ml <mitsuba@sfc.wide.ad.jp>
2014-09-07 04:45:47 +0900

    finish 9.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
+	before_action :signed_in_user, only:[:edit, :update]
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update_attributes(user_params)
+			flash[:success] = "Profile updated"
+			redirect_to @user
+		else
+			render 'edit'
+		end
+	end
+
 	def show
 		@user = User.find(params[:id])
 	end
@@ -21,5 +37,10 @@ class UsersController < ApplicationController
 	private
 		def user_params
 			params.require(:user).permit(:name, :email, :password, :password_confirmation)
+		end
+
+		# Before actions
+		def signed_in_user
+			redirect_to signin_url, notice:"Please sign in." unless signed_in?
 		end
 end
