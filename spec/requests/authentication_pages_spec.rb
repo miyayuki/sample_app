9798e073c5cbb864ee6ccae185068ac501f39585
melonsoda5ml <mitsuba@sfc.wide.ad.jp>
2014-09-07 06:52:36 +0900

    finish 9.2

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -29,13 +29,17 @@ describe "Authentication" do
 
 		describe "with valid information" do
 			let(:user){FactoryGirl.create(:user)}
+			before{sign_in user}
+=begin
 			before do
 				fill_in "Email",with: user.email.upcase
 				fill_in "Password",with: user.password
 				click_button "Sign in"
 			end
+=end
 
 			it {should have_title(user.name)}
+			it {should have_link('Users', href:users_path)}
 			it {should have_link('Profile', href: user_path(user))}
 			it {should have_link('Settings', href: edit_user_path(user))}
 			it {should have_link('Sign out', href: signout_path)}
@@ -53,10 +57,26 @@ describe "Authentication" do
 		describe "for non-signed-in users" do
 
 			let(:user){FactoryGirl.create(:user)}
+
+			describe "when attempting to visit a protected page" do
+				before do
+					visit edit_user_path(user)
+					fill_in "Email", with:user.email
+					fill_in "Password", with:user.password
+					click_button "Sign in"
+				end
+
+				describe "after signing in" do
+					it "should render the desired protected page" do
+						expect(page).to have_title('Edit user')
+					end
+				end
+			end
+
 			describe "in the Users controller" do
 
 				describe "visiting the edit page"do
-					before {visit edit_user_path(user)}
+					before {visit users_path}
 					it{should have_title('Sign in')}
 				end
 
