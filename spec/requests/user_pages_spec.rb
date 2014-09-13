acffe46d3f416cd7582335573b95954394a165f6
melonsoda5ml <mitsuba@sfc.wide.ad.jp>
2014-09-14 04:45:54 +0900

    remove error in rspec

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,12 +5,8 @@ describe "User Pages" do
 	subject { page }
 
 	describe "index" do
+		let!(:user){FactoryGirl.create(:user)}
 		before do
-=begin
-			sign_in FactoryGirl.create(:user)
-			FactoryGirl.create(:user, name:"Bob", email:"bob@example.com")
-			FactoryGirl.create(:user, name:"Beb", email:"ben@example.com")
-=end
 			sign_in user
 			visit users_path
 		end
@@ -36,17 +32,17 @@ describe "User Pages" do
 			describe "as an admin user" do
 				let(:admin) {FactoryGirl.create(:admin)}
 				before do
-					sign_in user
+					sign_in admin
 					visit users_path
 				end
 
-				it {should have_link('delete', href: user_path(User.first))}
+				it {should have_link('delete', href:user_path(User.first))}
 				it "should be able to delete another user" do
 					expect do
 						click_link('delete', match: :first)
 					end.to change(User, :count).by(-1)
 				end
-				it{should_not have_link('delete', href: user_path(admin))}
+				it{should_not have_link('delete', href:user_path(admin))}
 			end
 		end
 	end
@@ -140,6 +136,17 @@ describe "User Pages" do
 			it {should have_link('Sign out', href:signout_path)}
 			specify {expect(user.reload.name).to eq new_name}
 			specify {expect(user.reload.email).to eq new_email}
+		end
+
+		describe "forbidden attributes" do
+			let(:params) do
+				{user: {admin:true, password:user.password, password_confirmation: user.password}}
+			end
+			before do
+				sign_in user, no_capybara: true
+				patch user_path(user),params
+			end
+			specify {expect(user.reload).not_to be_admin}
 		end
 	end
 end
